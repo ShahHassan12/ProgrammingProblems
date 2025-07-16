dfb1073e7bf315329b0fa26939474df1d1359db5
Shah Hassan <sshahhassan99@gmail.com>
2025-07-15 23:24:44 -0400

    Updated implementation

--- dynamicArray.py
+++ dynamicArray.py
@@ -3,50 +3,36 @@ class DynamicArray:
     def __init__(self, capacity: int):
         self.array = [None]*capacity
         self.capacity = capacity
+        self.size = 0
 
     def get(self, i: int) -> int:
         return self.array[i]
 
     def set(self, i: int, n: int) -> None:
         self.array[i] = n
-        capacity -= 1
-
 
     def pushback(self, n: int) -> None:
-        if self.capacity <= 0:
+        if self.size >= self.capacity:
             self.resize()
-        self.array[len(self.array)-1] = n
-        self.capacity -= 1
 
+        self.array[self.size] = n
+        self.size += 1
 
     def popback(self) -> int:
-        temp = self.array[len(self.array)-1]
-        self.array[self.capacity-1] = None
-        self.capacity += 1
+        temp = self.array[self.size-1]
+        self.array[self.size-1] = None
+        self.size -= 1
         return temp
 
     def resize(self) -> None:
-        newArray = [None]*(self.capacity*2)
-
-        ix = 0
-
-        for elem in self.array:
-            newArray[ix] = elem
-            ix += 1
-        
+        self.capacity = self.capacity * 2
+        newArray = [None]*(self.capacity)
+        newArray[0:self.size] = self.array        
         self.array = newArray
         
-
-
     def getSize(self) -> int:
 
-        size = 0
-
-        for elem in self.array:
-            if not elem is None:
-                size += 1
-
-        return size
+        return self.size
         
     
     def getCapacity(self) -> int:
